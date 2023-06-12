4742c3e248c736ace57b49bccb9614981ff3779e
Dmitiy <dimapermyakov55@gmail.com>
2023-06-12 13:11:37 +0300

    Initial Commit

---
+++ MightyK1ngRichard-App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MightyK1ngRichard-App
+//
+//  Created by Дмитрий Пермяков on 12.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MightyK1ngRichard-App/MightyK1ngRichard_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MightyK1ngRichard_AppApp.swift
+//  MightyK1ngRichard-App
+//
+//  Created by Дмитрий Пермяков on 12.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct MightyK1ngRichard_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
