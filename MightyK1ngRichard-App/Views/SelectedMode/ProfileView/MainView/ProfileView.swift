57cbb4d2c6af905cf30885df8d8204be94657f99
Dmitiy <dimapermyakov55@gmail.com>
2023-06-13 01:36:06 +0300

    Update view

--- MightyK1ngRichard-App/Views/SelectedMode/ProfileView/MainView/ProfileView.swift
+++ MightyK1ngRichard-App/Views/SelectedMode/ProfileView/MainView/ProfileView.swift
@@ -8,8 +8,9 @@
 import SwiftUI
 
 struct ProfileView: View {
-    @EnvironmentObject var selected: SelectedButton
-
+    @EnvironmentObject var selected   : SelectedButton
+    @State private var pressedButton = "Фото"
+    
     var userPosts       = [
         ("2023-02-02", "Просто что-то про жизнь"),
         ("2023-02-02", "Просто что-то про жизнь"),
@@ -20,62 +21,25 @@ struct ProfileView: View {
     var nickname        = "Dmitriy Permyakov"
     var description     = " Engoing Web/iOS developing"
     var locationInfo    = "London"
+    var backroundImage  = "wwdc"
     var backgroundColor : UIColor = #colorLiteral(red: 0.1105830893, green: 0.1105830893, blue: 0.1105830893, alpha: 1)
     var colorOfText     : Color = .white
-    var countOfFriends  = "105" + "дрезей"
+    var countOfFriends  = "105" + " друзей"
+    
+    
     
     var body: some View {
         ScrollView {
-            VStack(spacing: 0) {
-                ZStack(alignment: .top) {
-                    Image("wwdc")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                    
-                    VStack {
-                        ZStack(alignment: .top) {
-                            Color(.black)
-                                .offset(y: 20)
-                            
-                            TopView()
-                            
-                            Image(image)
-                                .resizable()
-                                .aspectRatio(contentMode: .fill)
-                                .frame(width: 100, height: 100)
-                                .clipShape(Circle())
-                                .overlay {
-                                    Circle()
-                                        .stroke(lineWidth: 5)
-                                        .foregroundColor(Color(backgroundColor))
-                                }
-                                .padding(.bottom, 5)
-                                .offset(y: -50)
-                            // TODO: Убрать открытие меню по аве.
-                                .onTapGesture {
-                                    DispatchQueue.main.async {
-                                        withAnimation(.spring(response: 0.5, dampingFraction: 0.4)) {
-                                            selected.showMenu = true
-                                        }
-                                    }
-                                }
-                        }
-                        Spacer()
-                    }
-                    .padding(.top, 150)
-                }
-                
-                DownView()
-
-            }
+            ScreenOfUser()
         }
         .background(Color(.black))
         .ignoresSafeArea()
     }
     
+    @ViewBuilder
     private func TopView() -> some View {
-        ZStack(alignment: .bottom) {
-            VStack(spacing: 3) {
+        VStack {
+            VStack(spacing: 5) {
                 Text(nickname)
                     .font(.system(.headline, weight: .black))
                     .scaleEffect(1.3)
@@ -100,13 +64,13 @@ struct ProfileView: View {
                     Image(systemName: "info.circle")
                         .iconsSizes()
                 }
-                
             }
-            .foregroundColor(colorOfText)
-            .frame(maxWidth: .infinity, maxHeight: 200)
+            .padding()
+            .padding(.top, 40)
             
             Button {
                 // ?
+                
             } label: {
                 Text("Подписаться")
                     .padding(.vertical, 5)
@@ -120,13 +84,120 @@ struct ProfileView: View {
             .padding(.horizontal, 20)
             .padding(.bottom)
         }
+        .foregroundColor(colorOfText)
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
         .background(Color(backgroundColor))
         .cornerRadius(20)
-        
-        
     }
     
+    @ViewBuilder
+    private func ScreenOfUser() -> some View {
+        ZStack(alignment: .top) {
+            Image(backroundImage)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            VStack {
+                ZStack(alignment: .top) {
+                    Color(.black)
+                        .offset(y: 20)
+                    
+                    VStack {
+                        TopView()
+                            .frame(height: 200)
+                        DownView()
+                        
+                        imagesView()
+                    }
+                    
+                    Image(image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 100, height: 100)
+                        .clipShape(Circle())
+                        .overlay {
+                            Circle()
+                                .stroke(lineWidth: 5)
+                                .foregroundColor(Color(backgroundColor))
+                        }
+                        .padding(.bottom, 5)
+                        .offset(y: -50)
+                    // TODO: Убрать открытие меню по аве.
+                        .onTapGesture {
+                            DispatchQueue.main.async {
+                                withAnimation(.spring(response: 0.5, dampingFraction: 0.4)) {
+                                    selected.showMenu = true
+                                }
+                            }
+                        }
+                    
+                }
+            }
+            
+            .padding(.top, 150)
+        }
+    }
     
+    @ViewBuilder
+    private func imagesView() -> some View {
+        let buttons = [
+            ("Фото", "photo"),
+            ("Видео", "play.square"),
+            ("Музыка", "airpodspro"),
+            ("Клипы", "video"),
+        ]
+        
+        VStack {
+            // TODO: Сделать LazyHStack. Щас лень.
+            ScrollView(.horizontal, showsIndicators: false){
+                HStack {
+                    ForEach(buttons, id: \.self.0) {
+                        ButtonsOfModes(pressedButton: $pressedButton, img: $0.1, text: $0.0)
+                    }
+                }
+            }
+            .padding(.leading)
+            .padding(.top)
+            
+            HStack {
+                ForEach(0...2, id: \.self) { _ in
+                    Image("k1ng")
+                        .resizable()
+                        .frame(width: 120, height: 120)
+                }
+            }
+            
+            HStack {
+                Button {
+                    // ?
+                    
+                } label: {
+                    Image(systemName: "plus")
+                    Text("Загрузить фото")
+                }
+
+                
+                Divider()
+                
+                Button {
+                    // ?
+                    
+                } label: {
+                    Text("Загрузить фото")
+                    Image(systemName: "chevron.right")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 10, height: 10)
+                }
+            }
+            .padding(.bottom)
+        }
+        .foregroundColor(.white)
+        .frame(maxWidth: .infinity)
+        .background(Color(backgroundColor))
+        .cornerRadius(20)
+    }
+
     private func DownView() -> some View {
         VStack {
             Text(countOfFriends)
@@ -136,12 +207,37 @@ struct ProfileView: View {
                 .background(Color(backgroundColor))
                 .foregroundColor(colorOfText)
                 .cornerRadius(20)
-            
-            HStack {
-                
-            }
         }
     }
+}
+
+struct ButtonsOfModes: View {
+    @Binding var pressedButton: String
+    var img : String
+    var text: String
+    
+    
+    var body: some View {
+        let color = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        
+        HStack {
+            Button {
+                pressedButton = text
+                
+            } label: {
+                Image(systemName: img)
+                Text(text)
+            }
+
+        }
+        .padding(6)
+        .background(
+            Color(color)
+                .opacity(pressedButton == text ? 0.7 : 0)
+        )
+        .cornerRadius(10)
+    }
+    
 }
 
 struct ProfileView_Previews: PreviewProvider {
---
+++ README.md
@@ -0,0 +1,4 @@
+# K1ng-VK
+
+## Preview:
+<img class="screen_application" src="Preview/Preview.png" width="400">
